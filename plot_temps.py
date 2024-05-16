2905aaafb56739057cc519659069ffcf9727bc3e
Alex Belenkiy <51411108+stackOverSwag@users.noreply.github.com>
2024-05-16 14:44:06 +0200

    Update plot_temps.py

--- plot_temps.py
+++ plot_temps.py
@@ -1,18 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Ma touche personnelle :)
+plt.style.use('dark_background')
+
 # Chemin vers le fichier CSV contenant les temps d'exécution moyens
 csv_file_path = 'average_times.csv'
 
 # Lire le fichier CSV dans un DataFrame pandas
 # On suppose que le fichier a 4 colonnes : Taille du problème, Temps BT1, Temps BT2, Temps DP
-df = pd.read_csv(csv_file_path, header=None, names=['Taille', 'BT1', 'BT2', 'DP'])
+df = pd.read_csv(csv_file_path, header=None, names=['Taille', 'BT1', 'BT2', 'DP', 'DP_Value'])
 
 # Préparation du graphique pour comparer les temps d'exécution des trois algorithmes
 plt.figure(figsize=(10, 6))
-plt.plot(df['Taille'], df['BT1'], label='Backtracking 1', marker='o')
-plt.plot(df['Taille'], df['BT2'], label='Backtracking 2', marker='s')
-plt.plot(df['Taille'], df['DP'], label='Programmation Dynamique', marker='^')
+
+plt.semilogy(df['Taille'], df['BT1'], label='Backtracking 1', marker='o')
+plt.semilogy(df['Taille'], df['BT2'], label='Backtracking 2', marker='s')
+plt.semilogy(df['Taille'], df['DP'], label='Programmation Dynamique', marker='^')
+plt.semilogy(df['Taille'], df['DP_Value'], label='Programmation Dynamique (Value)', marker='n')
+
+# plt.plot(df['Taille'], df['BT1'], label='Backtracking 1', marker='o')
+# plt.plot(df['Taille'], df['BT2'], label='Backtracking 2', marker='s')
+# plt.plot(df['Taille'], df['DP'], label='Programmation Dynamique', marker='^')
+
 
 # Ajout de titres et légendes pour le graphique
 plt.title('Comparaison des temps d\'exécution des algorithmes pour le problème du sac à dos')
