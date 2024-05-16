c4448b4f2265413ad44578280829d4ffcbd5ee2b
Alex Belenkiy <51411108+stackOverSwag@users.noreply.github.com>
2024-05-16 18:05:37 +0200

    Update knapsack.c

--- knapsack.c
+++ knapsack.c
@@ -4,20 +4,34 @@
 #include <time.h>
 
 
-// Fonction utilitaire pour obtenir le maximum de deux nombres.
+// Fonctions utilitaires pour obtenir le max/min de deux nombres.
 int max(int a, int b) { return (a > b) ? a : b; }
 int min(int a, int b) { return (a < b) ? a : b; }
-
-int abs(int a) {
-    return (a < 0) ? -1 * a : a;
+// Fonction d'échange de deux éléments
+void swap(float* a, float* b) {
+    float t = *a;
+    *a = *b;
+    *b = t;
 }
-
 
 // Définition de la structure pour les objets.
 typedef struct {
     unsigned int weight;
     unsigned int value;
 } Item;
+
+void swapitem(Item* a, Item* b) {
+    Item t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int abs(int a) {
+    return (a < 0) ? -1 * a : a;
+}
+
+
+
 
 
 // Variables globales pour l'algorithme de backtracking
@@ -59,15 +73,10 @@ void knapsackBT1(int idx) {
             g_bestValue = max(g_bestValue, g_currentValue);
             return;
         }
-
         
-
         // sauter l'objet courant
         knapsackBT1(idx + 1);
-        
 
-        
-            
         // Inclure l'objet actuel s'il ne dépasse pas la capacité du sac à dos
         if (g_currentWeight + g_items[idx].weight <= g_W) {
             // Mettre à jour currentWeight et currentValue avant l'appel récursif
@@ -83,11 +92,6 @@ void knapsackBT1(int idx) {
         
     }
 }
-
-
-
-
-
 
 // Algorithme de backtracking sans variables globales
 int knapsackBTUtil(Item items[], int n, int W, int idx, int currentValue, int *bestValue) {
@@ -112,10 +116,6 @@ int knapsackBT2(Item items[], int n, int W) {
     int bestValue = 0;
     return knapsackBTUtil(items, n, W, 0, 0, &bestValue);
 }
-
-
-
-
 
 // Fonction de programmation dynamique pour le problème du sac à dos 0/1.
 int knapsackDP(int W, Item items[], int n) {
@@ -170,7 +170,74 @@ int knapsackDP_Value(int W, Item items[], int n) {
     return max_value;
 }
 
+int knapsackDP_Matrix(int W, int M, Item items[], int n) {
+    int **dp = (int**)malloc((n + 1) * sizeof(int*));
+    for(int i = 0; i <= n; i++) {
+        dp[i] = (int*)malloc((W + 1) * sizeof(int));
+    }
 
+    for(int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (items[i-1].weight <= w)
+                dp[i][w] = max(items[i-1].value + dp[i-1][w-items[i-1].weight], dp[i-1][w]);
+            else
+                dp[i][w] = dp[i-1][w];
+        }
+    }
+
+    int result = dp[n][W];
+    for (int i = 0; i <= n; i++)
+        free(dp[i]);
+    free(dp);
+    return (result >= M) ? result : -1;
+
+}
+
+int partition(Item items[], float arr[], int low, int high) {
+    float pivot = arr[high];
+    int i = (low - 1); // Index du plus petit élément
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+            swapitem(&items[i], &items[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    swapitem(&items[i + 1], &items[high]);
+    return (i + 1);
+}
+
+// Fonction principale qui implémente quicksort
+void quickSort(Item items[], float arr[], int low, int high) {
+    if (low < high) {
+        // pi est l'index de partition, arr[pi] est maintenant à la bonne position
+        int pi = partition(items, arr, low, high);
+        // Trier les éléments séparément avant et après la partition
+        quickSort(items, arr, low, pi - 1);
+        quickSort(items, arr, pi + 1, high);
+    }
+}
+
+int knapsackGlouton(int W, Item items[], int n) {
+    float arr[n + 1];
+    for (int i = 0; i < n; i++) {
+        arr[i] = items[i].value / items[i].weight;
+    }
+    quickSort(items, arr, 1, n);
+    int totalW = W, totalV = 0;
+
+    for(int i = n - 1; i >= 0; i--) {
+        if (items[i].weight <= totalW) {
+            totalW -= items[i].weight;
+            totalV += items[i].value;
+        }
+    }
+    return totalV;
+}
 
 
 int main() {
@@ -183,24 +250,30 @@ int main() {
     }
 
     // Variables pour stocker les somme des temps et compter les instances par taille de problème
-    double sumTimeBT1 = 0, sumTimeBT2 = 0, sumTimeDP = 0, sumTimeDP_Value = 0;
+    double sumTimeBT1 = 0, sumTimeBT2 = 0, sumTimeDP = 0, sumTimeDP_Value = 0, sumTimeDP_Matrix = 0, sumTimeGL;
     int count = 0, currentN = -1;
 
     char line[4096];
     while (fgets(line, 4096, fp)) {
-        int W, n;
-        sscanf(line, "%d,%d", &W, &n);
+        int W, M, n;
+        sscanf(line, "%d,%d,%d", &W, &M, &n);
 
         // Vérifiez si nous passons à une nouvelle taille de problème
         if (n != currentN) {
             if (count > 0) {
                 // Écrivez les moyennes pour la taille de problème précédente
-                fprintf(out, "%d, %f, %f, %f, %f\n",
-                        currentN, sumTimeBT1 / count, sumTimeBT2 / count, sumTimeDP / count, sumTimeDP_Value / count);
+                fprintf(out, "%d, %f, %f, %f, %f, %f, %f\n",
+                        currentN, 
+                        sumTimeBT1 / count, 
+                        sumTimeBT2 / count, 
+                        sumTimeDP / count, 
+                        sumTimeDP_Value / count, 
+                        sumTimeDP_Matrix / count,
+                        sumTimeGL / count);
             }
             // Réinitialisez les compteurs pour la nouvelle taille
             currentN = n;
-            sumTimeBT1 = sumTimeBT2 = sumTimeDP = sumTimeDP_Value = 0;
+            sumTimeBT1 = sumTimeBT2 = sumTimeDP = sumTimeDP_Value = sumTimeDP_Matrix = sumTimeGL = 0;
             count = 0;
         }
 
@@ -250,16 +323,40 @@ int main() {
         double timeDP_Value = (double)(end - start) / CLOCKS_PER_SEC;
         sumTimeDP_Value += timeDP_Value;
 
+        start = clock();
+        int maxValDP_Matrix = knapsackDP_Matrix(W, M, items, n);
+        end = clock();
+        double timeDP_Matrix = (double)(end - start) / CLOCKS_PER_SEC;
+        sumTimeDP_Matrix += timeDP_Matrix;
+
+        start = clock();
+        int maxValGL = knapsackGlouton(W, items, n);
+        end = clock();
+        double timeGL = (double)(end - start) / CLOCKS_PER_SEC;
+        sumTimeGL += timeGL;
+
         count++;
 
-        fprintf(sol, "%d, %d, %f, %d, %f, %d, %f, %d\n",
-            n, W, timeBT1, maxValBT1, timeBT2, maxValBT2, timeDP, maxValDP);
+        fprintf(sol, "%d, %d, %f, %d, %f, %d, %f, %d, %f, %d, %f, %d, %f, %d\n",
+            n, W, 
+            timeBT1, maxValBT1, 
+            timeBT2, maxValBT2, 
+            timeDP, maxValDP, 
+            timeDP_Value, maxValDP_Value, 
+            timeDP_Matrix, maxValDP_Matrix,
+            timeGL, maxValGL);
     }
 
     // N'oubliez pas d'écrire les moyennes pour la dernière taille de problème
     if (count > 0) {
-        fprintf(out, "%d, %f, %f, %f, %f\n",
-                currentN, sumTimeBT1 / count, sumTimeBT2 / count, sumTimeDP / count, sumTimeDP_Value / count);
+        fprintf(out, "%d, %f, %f, %f, %f, %f\n",
+                currentN, 
+                sumTimeBT1 / count, 
+                sumTimeBT2 / count, 
+                sumTimeDP / count, 
+                sumTimeDP_Value / count,
+                sumTimeDP_Matrix / count,
+                sumTimeGL / count);
     }
 
     fclose(fp);
